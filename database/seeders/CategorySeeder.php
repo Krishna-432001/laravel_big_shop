50814c31f1f163de85fcf6ba2a73a0b13f49f24e
krishna-432001 <krishnapriya432001@gmail.com>
2024-08-12 13:24:50 +0530

    Update CategorySeeder.php

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use App\Models\Category;
+
 class CategorySeeder extends Seeder
 {
     /**
@@ -14,6 +16,26 @@ class CategorySeeder extends Seeder
     {
         //
 
+        $categories = [
+            ['name' => 'Fresh Produce'],
+            ['name' => 'Dairy and Eggs'],
+            ['name' => 'Meat and Seafood'],
+            ['name' => 'Bakery'],
+            ['name' => 'Pantry Staples'],
+            ['name' => 'Beverages'],
+            ['name' => 'Frozen Foods'],
+            ['name' => 'Snacks'],
+            ['name' => 'Health and Wellness'],
+            ['name' => 'Household Supplies'],
+            ['name' => 'Personal Care'],
+            ['name' => 'Baby Products'],
+            ['name' => 'Pet Supplies'],
+            ['name' => 'International Foods'],
+        ];
+
+        foreach ($categories as $row) {
+            Category::create($row);
+        }
        
     }
 }
