bf4befb43a22eb14e901f0ee4302c7e6484e1ab9
krishna-432001 <krishnapriya432001@gmail.com>
2024-08-14 21:09:01 +0530

    category and city model added and condition added

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,11 +4,30 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Category;
+
+use App\Models\City;
+
+
 class AuthController extends Controller
 {
     public function login(Request $request)
     {
-        return view('frontend/auth/login');
+
+        // dd($request);
+        // Retrieve all cities for use in the view
+        $cities = City::all();
+
+        // Retrieve all categories for use in the view
+        $categories = Category::all();
+
+
+        // Initialize data array
+        $data = [
+            'categories' => $categories,
+            'cities' => $cities,
+         ];
+        return view('frontend/auth/login',$data);
     }
 
     public function register(Request $request)
