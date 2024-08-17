5dd62b71edb028c20ab64d7f4fdb3eaf4193bc9c
krishna-432001 <krishnapriya432001@gmail.com>
2024-08-17 13:55:47 +0530

    cart display user id to see the cart screen logic and total price qty added

--- resources/views/frontend/layout/header.blade.php
+++ resources/views/frontend/layout/header.blade.php
@@ -112,42 +112,34 @@
                                 </a>
                                 <a href="shop-wishlist.html"><span class="lable">Wishlist</span></a>
                             </div>
+                            @if (Auth::check())
                             <div class="header-action-icon-2">
                                 <a class="mini-cart-icon" href="shop-cart.html">
                                     <img alt="Nest" src="{{ asset('frontend/imgs/theme/icons/icon-cart.svg') }}" />
-                                    <span class="pro-count blue">2</span>
+                                    <span class="pro-count blue">{{ Auth::user()->cart()->count() }}</span>
                                 </a>
                                 <a href="shop-cart.html"><span class="lable">Cart</span></a>
                                 <div class="cart-dropdown-wrap cart-dropdown-hm2">
                                     <ul>
+                                        
+                                        @foreach(Auth::user()->cart as $row)
                                         <li>
                                             <div class="shopping-cart-img">
                                                 <a href="shop-product-right.html"><img alt="Nest" src="{{ asset('frontend/imgs/shop/thumbnail-3.jpg') }}" /></a>
                                             </div>
                                             <div class="shopping-cart-title">
-                                                <h4><a href="shop-product-right.html">Daisy Casual Bag</a></h4>
-                                                <h4><span>1 × </span>$800.00</h4>
+                                                <h4><a href="shop-product-right.html">{{ $row->product->name }}</a></h4>
+                                                <h4><span>{{ $row->qty }} × </span>₹{{ $row->product->price }}</h4>
                                             </div>
                                             <div class="shopping-cart-delete">
                                                 <a href="#"><i class="fi-rs-cross-small"></i></a>
                                             </div>
                                         </li>
-                                        <li>
-                                            <div class="shopping-cart-img">
-                                                <a href="shop-product-right.html"><img alt="Nest" src="{{ asset('frontend/imgs/shop/thumbnail-2.jpg') }}" /></a>
-                                            </div>
-                                            <div class="shopping-cart-title">
-                                                <h4><a href="shop-product-right.html">Corduroy Shirts</a></h4>
-                                                <h4><span>1 × </span>$3200.00</h4>
-                                            </div>
-                                            <div class="shopping-cart-delete">
-                                                <a href="#"><i class="fi-rs-cross-small"></i></a>
-                                            </div>
-                                        </li>
+                                        @endforeach
                                     </ul>
                                     <div class="shopping-cart-footer">
                                         <div class="shopping-cart-total">
-                                            <h4>Total <span>$4000.00</span></h4>
+                                            <h4>Total <span>₹ {{ $row->grandTotal(Auth::user()->id) }}</span></h4>
                                         </div>
                                         <div class="shopping-cart-button">
                                             <a href="shop-cart.html" class="outline">View cart</a>
@@ -156,6 +148,7 @@
                                     </div>
                                 </div>
                             </div>
+                            @endif
                             <div class="header-action-icon-2">
                                 <a href="page-account.html">
                                     <img class="svgInject" alt="Nest" src="{{ asset('frontend/imgs/theme/icons/icon-user.svg') }}" />
--- resources/views/frontend/product/detail/type1.blade.php
+++ resources/views/frontend/product/detail/type1.blade.php
@@ -77,10 +77,10 @@ product detail
                                 </div>
                                 <div class="clearfix product-price-cover">
                                     <div class="product-price primary-color float-left">
-                                        <span class="current-price text-brand">${{$product->price}}</span>
+                                        <span class="current-price text-brand">₹{{$product->price}}</span>
                                         <span>
                                             <span class="save-price font-md color3 ml-15">26% Off</span>
-                                            <span class="old-price font-md ml-15">${{$product->price}}</span>
+                                            <span class="old-price font-md ml-15">₹{{$product->price}}</span>
                                         </span>
                                     </div>
                                 </div>
